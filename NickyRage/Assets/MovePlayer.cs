b83ef390cb71d81fd7ec726b2895b06d8c101bab
Jeremy Vercillo <icelax1992@comcast.net>
2014-04-06 16:23:51 -0400

    Added physics to the jumping animation. Rigidbody's physics seems a bit low though so I might manually make it stronger.

--- NickyRage/Assets/MovePlayer.cs
+++ NickyRage/Assets/MovePlayer.cs
@@ -5,7 +5,8 @@ public class MovePlayer : MonoBehaviour {
 
 	public Animator animControl;
 	static int jumpState = Animator.StringToHash("Base.Jump"); 
-	
+	int jumptimer = 0;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -13,6 +14,7 @@ public class MovePlayer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		GameObject nicky = GameObject.Find ("NickyRage");
 
 		bool up = Input.GetKey (KeyCode.W);
 		bool down = Input.GetKey (KeyCode.S);
@@ -28,13 +30,21 @@ public class MovePlayer : MonoBehaviour {
 		else if (down) {
 			animControl.Play("WalkingBackward");
 		}
-		else if (space){
+		else if (space && currentBaseState.nameHash != jumpState){
 			animControl.Play("Jump");
+			jumptimer = 0;
 		}
+		else if(currentBaseState.nameHash == jumpState) {
+			jumptimer++;
+			if(jumptimer == 16) {
+				nicky.rigidbody.AddForce(new Vector3(0.0f, 15.0f, 0.0f), ForceMode.Impulse);
+			}
+		} // times the jump with the animation
 		else if (currentBaseState.nameHash != jumpState){
 			animControl.Play("Idle");
 		}
 
-		Debug.Log(animControl.GetCurrentAnimatorStateInfo(0));
+//		Debug.Log(animControl.GetCurrentAnimatorStateInfo(0));
+//		Debug.Log (nicky.rigidbody.velocity);
 	}
 }
