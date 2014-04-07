2e772348e805a83bf4da0988697ea3ee6c4bc454
jquinn630 <jquinn181@gmail.com>
2014-04-07 15:28:15 -0400

    Added Walking, Rotating, and Jumping Forward

---
+++ NickyRage/Assets/FollowScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowScript : MonoBehaviour {
+
+	public Transform follow;
+
+	// Use this for initialization
+	void Start () {
+		follow = GameObject.Find ("NickyRage").transform;
+		transform.position = follow.position;
+
+		Transform camera = GameObject.Find ("Main Camera").transform;
+		camera.position = new Vector3 (transform.position.x, transform.position.y + 10, transform.position.z + 10); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = follow.position;
+	}
+}
--- NickyRage/Assets/MovePlayer.cs
+++ NickyRage/Assets/MovePlayer.cs
@@ -7,9 +7,16 @@ public class MovePlayer : MonoBehaviour {
 	static int jumpState = Animator.StringToHash("Base.Jump"); 
 	int jumptimer = 0;
 
+	//movement stuffs
+	float moveMagConst;
+	Vector3 moveDirection;
+	float rotDeg;
+
 	// Use this for initialization
 	void Start () {
-	
+		moveMagConst= 7.5f;
+		moveDirection = new Vector3 (0f, 0f, 1f);
+		rotDeg = 120f;
 	}
 	
 	// Update is called once per frame
@@ -23,26 +30,47 @@ public class MovePlayer : MonoBehaviour {
 		bool space = Input.GetKey (KeyCode.Space);
 
 		AnimatorStateInfo currentBaseState = animControl.GetCurrentAnimatorStateInfo(0);
+		float moveMagnitude = moveMagConst * Time.deltaTime;
 
-		if (up) {
+		if (up && currentBaseState.nameHash != jumpState) {
+			transform.position -= new Vector3(moveDirection.x * moveMagnitude, 0f, moveDirection.z * moveMagnitude);
 			animControl.Play("WalkingForward");
 		}
+		else if (up && currentBaseState.nameHash == jumpState)
+		{
+			transform.position -= new Vector3(moveDirection.x * moveMagnitude, 0f, moveDirection.z * moveMagnitude);
+		}
 		else if (down) {
+			transform.position += new Vector3(moveDirection.x * moveMagnitude, 0f, moveDirection.z * moveMagnitude);
 			animControl.Play("WalkingBackward");
 		}
 		else if (space && currentBaseState.nameHash != jumpState){
 			animControl.Play("Jump");
 			jumptimer = 0;
 		}
-		else if(currentBaseState.nameHash == jumpState) {
+		else if (currentBaseState.nameHash != jumpState){
+			animControl.Play("Idle");
+		}
+		
+		if (left  && currentBaseState.nameHash != jumpState) {
+			moveDirection = Quaternion.Euler(0, -rotDeg * Time.deltaTime, 0) * moveDirection; 
+			transform.RotateAround(transform.position, Vector3.up, -rotDeg*Time.deltaTime);
+			moveDirection.Normalize();
+			animControl.Play("Run");
+		}
+		if (right  && currentBaseState.nameHash != jumpState) {
+			moveDirection = Quaternion.Euler (0, rotDeg * Time.deltaTime, 0) * moveDirection; 
+			transform.RotateAround (transform.position, Vector3.up, rotDeg * Time.deltaTime);
+			moveDirection.Normalize();
+			animControl.Play("Run");
+		}
+		if(currentBaseState.nameHash == jumpState) {
 			jumptimer++;
 			if(jumptimer == 16) {
 				nicky.rigidbody.AddForce(new Vector3(0.0f, 15.0f, 0.0f), ForceMode.Impulse);
 			}
 		} // times the jump with the animation
-		else if (currentBaseState.nameHash != jumpState){
-			animControl.Play("Idle");
-		}
+
 
 //		Debug.Log(animControl.GetCurrentAnimatorStateInfo(0));
 //		Debug.Log (nicky.rigidbody.velocity);
