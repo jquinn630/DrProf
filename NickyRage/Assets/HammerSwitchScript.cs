24fa9e77fc8193dd711ebe1ef954b130a981d85f
jquinn630 <jquinn181@gmail.com>
2014-04-22 02:47:35 -0400

    added puzzle to first room of norse

---
+++ NickyRage/Assets/HammerSwitchScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HammerSwitchScript : MonoBehaviour {
+
+	public GameObject targetObj;
+	public GameObject weapon;
+	bool isActive;
+
+	// Use this for initialization
+	void Start () {
+		isActive = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter(Collision other) {
+	
+		if (other.gameObject.name == weapon.name)
+		{
+			isActive = true;
+			foreach (AnimationState clip in targetObj.animation)
+			{
+				targetObj.animation.Play(clip.name);
+			}
+			gameObject.renderer.material.color = Color.red;
+		}
+	}
+}
---
+++ NickyRage/Assets/Room1PuzzleWin.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Room1PuzzleWin : MonoBehaviour {
+
+	public GameObject Door;
+	bool isSolved;
+	
+	// Use this for initialization
+	void Start () {
+		isSolved = false;
+		Door = GameObject.Find("Room1/DoorParent1/Door");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int one = GameObject.Find("Tile6").GetComponent<TileActive>().isTileOn;
+		int two = GameObject.Find("Tile7").GetComponent<TileActive>().isTileOn;
+		int three = GameObject.Find("Tile8").GetComponent<TileActive>().isTileOn;
+		int four = GameObject.Find("Tile9").GetComponent<TileActive>().isTileOn;
+		
+		if (one==1 && two==1 && three ==1 && four ==1 && !isSolved)
+		{
+			isSolved = true;
+			foreach (AnimationState clip in Door.animation)
+			{
+				Door.animation.Play(clip.name);
+			}
+		}
+		
+	}
+}
